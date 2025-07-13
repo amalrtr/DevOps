8650d615fb13769453fc20df9b987fef9b804d1e
amalrtr <rajamaltr@gmail.com>
2025-07-13 17:03:30 +0530

    br4: CICD Project

---
+++ proj_4/TF_code/terraform.tfvars
@@ -0,0 +1,3 @@
+instance_type = "t2.large"
+ami_id        = "ami-0f918f7e67a3323f0"
+instance_name = "cicd-instance"
---
+++ proj_4/TF_code/variables.tf
@@ -0,0 +1,17 @@
+variable "instance_type" {
+  default = "t2.micro"
+  type    = string
+}
+
+variable "ami_id" {
+  type = string
+
+  validation {
+    condition     = length(var.ami_id) > 4 && substr(var.ami_id, 0, 4) == "ami-"
+    error_message = "Entered AMI ID is invalid, enter correct AMI ID"
+  }
+}
+
+variable "instance_name" {
+    type = string
+} 
